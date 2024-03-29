3bd16eca8449abb98339bf06d04cc110979633cf
ashikale <124694837+ashikale@users.noreply.github.com>
2024-03-29 12:19:28 +0530

    Create Jfrog.sh

---
+++ weshopify-platform-app/Jfrog.sh
@@ -0,0 +1,51 @@
+ pipeline {
+    agent any
+    environment{
+        CI= true
+        JROG_CREDENTIALS= credentials('jfrog-credentials')
+    }
+ 
+ tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "MAVEN_HOME"
+    }
+
+    stages{
+        stage('pull code from git scm'){
+         steps{
+             echo '=======pull the code from gir scm========'
+             git branch: 'main',
+             url: 'https://github.com/ashikale/ValidationPractise.git'
+             echo '=======source code pulled========'
+         }
+    }
+     stage('Build the source code'){
+         steps{
+             echo '=======code building is starting========'
+             bat 'cd weshopify-platform-app '
+             bat 'cd weshopify-platform-app && mvn clean package'
+             echo '=======JAR file generated========'
+         }
+    }
+    stage('deploy to frog'){
+         steps{
+             echo '=======jrog building is starting========'
+
+             bat 'cd weshopify-platform-app && mvn deploy'
+             echo '=======JAR file transfer to JFROG========'
+         }
+    }
+
+    }
+    stage('copy jfrog.sh file to docker machine'){
+         steps{
+             echo '=======jrog.sh file copy started========'
+
+             bat 'cd weshopify-platform-app && copy Jfrog.sh Sandesh@192.168.31.92:D:\Docker'
+             echo '=======Jfrog filed copied========'
+         }
+    }
+
+    }
+
+}
